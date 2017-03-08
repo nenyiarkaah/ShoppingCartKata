8a41d75f8abdec00478e762ab49b36b5a4e52d30
narkaah <nenyi.arkaah@gmail.com>
2017-03-08 06:03:49 +0000

    Now with functions as parameters.

--- shopping/src/main/scala/CheckOut.scala
+++ shopping/src/main/scala/CheckOut.scala
@@ -8,7 +8,7 @@ class CheckOut(shoppingBasket: List[ProductItem]) extends Inventory with Special
 
   def totalCostOfBasket = basket.map(_.price).sum
 
-  def validSpecialOffers = (for (s <- SpecialOffers if (doesSpecialOfferApply(s))) yield applySpecialOfferApply(s)).toList.flatten
+  def validSpecialOffers = (for (s <- SpecialOffers if (applySpecialOfferFunction[Boolean, Boolean](s, doesOfferApplyList, isOfferApplicable))) yield applySpecialOfferFunction[Int, IndexedSeq[Int]](s, offerApply, sameOffersList)).toList.flatten.asInstanceOf[List[OfferItem]]
 
   private def applySpecialOffersToBasket(validSpecialOffers: List[OfferItem]) = {
     val validSpecialOffersTotal = validSpecialOffers.map(v => {
@@ -17,28 +17,37 @@ class CheckOut(shoppingBasket: List[ProductItem]) extends Inventory with Special
     BigDecimal(totalCostOfBasket - validSpecialOffersTotal).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
   }
 
-  private def doesSpecialOfferApply(offer: OfferItem) = {
-    val basketGroups = basket.groupBy(_.name).mapValues(_.size).toSeq
-    val offerGroups = offer.products.groupBy(_.name).mapValues(_.size).toSeq
-    val isTrue = offerGroups.map(item => {
-      val basketProductGroup = basketGroups.filter(_._1 == item._1).head
-      val (product, productQuantity) = basketGroups.filter(_._1 == item._1).headOption match {
-        case None => (None, 0)
-        case Some(x) => (x, x._2)
-      }
-      if (item._2 <= productQuantity) true else false
-    })
-    !isTrue.contains(false)
+  private def extractProductGroupings(list: List[ProductItem]) = list.groupBy(_.name).mapValues(_.size).toSeq
+
+  private def applySpecialOfferFunction[T, S](offer: OfferItem, offerFunction: (Seq[(String, Int)], Seq[(String, Int)]) => Seq[Any], applyFunction: (OfferItem, Seq[T]) => Any) = {
+    val basketGroups = extractProductGroupings(basket)
+    val offerGroups = extractProductGroupings(offer.products)
+    val offerResult = offerFunction(offerGroups, basketGroups).asInstanceOf[Seq[T]]
+    applyFunction(offer, offerResult).asInstanceOf[S]
   }
 
-  private def applySpecialOfferApply(offer: OfferItem) = {
-    val basketGroups = basket.groupBy(_.name).mapValues(_.size).toSeq
-    val offerGroups = offer.products.groupBy(_.name).mapValues(_.size).toSeq
-    val noOfSameOffer = offerGroups.map{case (item: String, numberOfItems: Int) => {
+  private val isOfferApplicable = (offer: OfferItem, validProducts: Seq[Boolean]) => !validProducts.contains(false)
+  private val sameOffersList = (offer: OfferItem, noOfSameOffer: Seq[Int]) => for (i <- 1 to noOfSameOffer.min) yield offer
+  private val doesOfferApplyList = (offerGroups: Seq[(String, Int)], basketGroups: Seq[(String, Int)]) => {
+    offerGroups.map(item => {
+      doesOfferApply(item._1, item._2, basketGroups)
+    })
+  }
+
+  private val doesOfferApply = (name: String, quantity: Int, basketGroups: Seq[(String, Int)]) => {
+    val basketProductGroup = basketGroups.filter(_._1 == name).head
+    val (_, productQuantity) = basketGroups.filter(_._1 == name).headOption match {
+      case None => (None, 0)
+      case Some(productGroup) => (productGroup, productGroup._2)
+    }
+    if (quantity <= productQuantity) true else false
+  }
+
+  private val offerApply = (offerGroups: Seq[(String, Int)], basketGroups: Seq[(String, Int)]) => {
+    offerGroups.map { case (item: String, numberOfOffers: Int) => {
       val head = basketGroups.filter(_._1 == item).head
-      head._2 / numberOfItems
+      head._2 / numberOfOffers
     }}
-    for (i <- 1 to noOfSameOffer.min) yield offer
   }
 
   def totalCostOfBasketWithDiscount = applySpecialOffersToBasket(validSpecialOffers)
